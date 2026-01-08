624dc60b2eb80e1a431801fd46af30507acebc45
ne5ermind <stepankutaev@yandex.ru>
2026-01-08 16:46:37 +0300

    fixed lazy trying to create lockfile in a system directory

--- lua/plugins/lazy-init.lua
+++ lua/plugins/lazy-init.lua
@@ -46,6 +46,7 @@ require("lazy").setup({
 	require("plugins.lsp"),
 	require("plugins.lint"),
 }, {
+    lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
 		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
